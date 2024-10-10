658f459d3dd791a23c317b0dd5eaf669be07ca3b
shamsudeen Ibrahim <shamsain3@gmail.com>
2024-10-10 14:23:02 +0000

    adjusted according to pycode style

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
+"""
+canUnlocAll(boxes)
+"""
+
 
 def canUnlockAll(boxes):
-    # Start by unlocking box 0
-    unlocked = {0}  # Set of unlocked boxes, initially only box 0 is unlocked
-    keys = set(boxes[0])  # Keys found in the first box
-    to_check = [0]  # Boxes that we need to explore, starting with box 0
 
-    # Continue unlocking boxes until there are no more boxes to check
+    unlocked = {0}
+    keys = set(boxes[0])
+    to_check = [0]
+
     while to_check:
-        current_box = to_check.pop()  # Take a box to check
-        
-        # Check all keys in the current box
+        current_box = to_check.pop()
+
         for key in boxes[current_box]:
-            if key < len(boxes) and key not in unlocked:  # If valid and not unlocked
-                unlocked.add(key)  # Unlock the box
-                to_check.append(key)  # Add to boxes to explore
-                keys.update(boxes[key])  # Collect new keys from the newly unlocked box
-    
-    # If the number of unlocked boxes equals the total number of boxes, return True
+            if key < len(boxes) and key not in unlocked:
+                unlocked.add(key)
+                to_check.append(key)
+                keys.update(boxes[key]) 
+
     return len(unlocked) == len(boxes)
